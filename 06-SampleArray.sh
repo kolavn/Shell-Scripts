7217c9946fe65cf5cd99530ebf4713c0c47fa92c
kolavn <veda.kola@gmail.com>
2024-08-28 23:10:07 -0400

    Sample Array

--- 06-SampleArray.sh
+++ 06-SampleArray.sh
@@ -2,4 +2,6 @@
 
 family=("Dad" "Mom" "Sister")
 
-echo "The funniest in the family is: $family[2]"
+echo "The first one is: ${family[0]}"
+echo "The second one is: ${family[1]}"
+echo "The third one is: ${family[2]}"
