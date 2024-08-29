bf8b5c0fe5cfa864d0465755f5b9c7ffe3ca24d2
kolavn <veda.kola@gmail.com>
2024-08-29 02:03:28 -0400

    Creating Logs file for script

---
+++ 16-Redirectors.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Program to create a folder for shell-script logs in /var/logs/shell-scripts/16-Redirectors.sh-<timestamp>.log
+
+# Logs are important as to know what's the status of any action Succes/Failure.
+
+# By creating this folder we can know when we ran the script &  whats the output, what's the error in the code
+
+### echo "16-Redirectors.sh" | cut -d "." -f1   or  echo "$0" | cut -d "." "-f1"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo "$0" | cut -d "." "-f1")
+TIME_STAMP=$(date +%Y-%m-%d-%H-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log" 
+
+mkdir -p $LOGS_FOLDER
+ 
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges N" &>>$LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+        exit 1
+    else 
+        echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE
+    fi  
+}
+
+CHECK_ROOT
