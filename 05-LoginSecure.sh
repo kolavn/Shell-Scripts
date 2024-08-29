d544c598e27ba7f66bb1c2f5823c78a2f44a65c5
kolavn <veda.kola@gmail.com>
2024-08-28 23:01:30 -0400

    Username and Password Reading without visibility to others

---
+++ 05-LoginSecure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Plese enter username"
+
+read -s username #this helps in not showing what we entered"
+
+echo "The entered username is $username"
+
+echo "Enter Password"
+
+read -s password #this helps in not showing what we entered"
+
+echo "The entered password is $password"
