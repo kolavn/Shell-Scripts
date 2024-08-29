84dd823a58616c7550a5c004c8a860014e389853
kolavn <veda.kola@gmail.com>
2024-08-28 23:11:58 -0400

    Sample Array

--- 06-SampleArray.sh
+++ 06-SampleArray.sh
@@ -2,6 +2,6 @@
 
 family=("Dad" "Mom" "Sister")
 
-echo "The first one is: $family[0]"
-echo "The second one is: $family[1]"
-echo "The third one is: $family[2]"
+echo "The first one is: ${family[0]}"
+echo "The second one is: ${family[1]}"
+echo "The third one is: ${family[2]}"
