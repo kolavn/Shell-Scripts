9374b5b9bcbc78c116595064099832c48a4df337
kolavn <veda.kola@gmail.com>
2024-08-28 22:57:31 -0400

    Username and Password Reading

---
+++ 04-LoginDetails.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Plese enter username"
+
+read username
+
+echo "The entered username is $username"
+
+echo "Enter Password"
+
+read password
+
+echo "The entered password is $password"
